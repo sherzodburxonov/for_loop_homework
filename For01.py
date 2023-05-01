0826c44cf8afc52b3355f41e9fe5d4df32a7b9e0
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 17:30:19 +0500

    01

--- For01.py
+++ For01.py
@@ -1,6 +1,3 @@
-import py_compile
-
-
 def main(n):
     """
     Return numbers from zero to n in a list view.
@@ -9,4 +6,6 @@ def main(n):
     Returns:
         list: return  answer
     """
-    return
+
+    return list(range(n))
+print(main(5))
