7d74e4b121f07fd8cd195dbd84b6fb5500d9eea7
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-02 10:44:13 +0500

    07

--- For07.py
+++ For07.py
@@ -6,4 +6,9 @@ def main(N):
     Returns:
         int: return  answer
     """
-    return
+    sum=0
+    for i in range(N+1):
+        if i%2==1:
+            sum=sum+i
+    return sum
+print(main(12))
